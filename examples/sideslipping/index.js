ad6244796d22a1b8b3c92c243201cb0b35d878dc
wangheng3751 <693184528@qq.com>
2018-01-17 10:24:51 +0800

    更新滑动删除实例

--- examples/sideslipping/index.js
+++ examples/sideslipping/index.js
@@ -1,10 +1,10 @@
  window.onload=function(){
-
+    setSlide();
  };
- function setSlide(selector) {
+ function setSlide() {
     //侧滑显示删除按钮
     var expansion = null; //是否存在展开的list
-    var container = document.querySelectorAll(selector);
+    var container = document.querySelectorAll(".swipe-container");
     for (var i = 0; i < container.length; i++) {
         var x, y, X, Y, swipeX, swipeY;
         container[i].addEventListener('touchstart', function (event) {
@@ -13,7 +13,8 @@
             swipeX = true;
             swipeY = true;
             if (expansion) { //判断是否展开，如果展开则收起
-                $(this).removeClass("swipeleft");
+                removeClass(this,"swipeleft");
+                //$(this).removeClass("swipeleft");
             }
         });     
         container[i].addEventListener('touchmove', function (event) {
@@ -23,15 +24,15 @@
             if (swipeX && Math.abs(X - x) - Math.abs(Y - y) > 0) {
                 // 阻止事件冒泡
                 event.stopPropagation();
-                if (X - x > 10) { //右滑
+                if (X - x > 10) { //右滑收起
                     event.preventDefault();
-                    $(this).removeClass("swipeleft");
-                    //this.className = ""; //右滑收起
+                    removeClass(this,"swipeleft");
+                    //$(this).removeClass("swipeleft");
                 }
-                if (x - X > 10) { //左滑
+                if (x - X > 10) { //左滑展开
                     event.preventDefault();
-                    $(this).addClass("swipeleft");
-                    //this.className = "swipeleft"; //左滑展开
+                    //$(this).addClass("swipeleft");
+                    addClass(this,"swipeleft");
                     expansion = this;
                 }
                 swipeY = false;
@@ -43,3 +44,25 @@
         });
     }
 }
+
+function hasClass(elem, cls) {
+    cls = cls || '';
+    if (cls.replace(/\s/g, '').length == 0) return false;
+    return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
+  }
+   
+  function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) {
+      ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
+    }
+  }
+   
+  function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+      var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
+      while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+        newClass = newClass.replace(' ' + cls + ' ', ' ');
+      }
+      ele.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+  }
